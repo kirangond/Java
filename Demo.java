2c8bb6918d28e1acde08fcd0110f9e6912cc2803
kirangond <kirangond05@gmail.com>
2021-03-23 23:34:50 +0530

    Add files via upload
    void return type method

---
+++ Demo.java
@@ -0,0 +1,31 @@
+package methods;
+
+public class Demo {
+	// behavioral of class & void return type method
+        void study()
+        {
+        	System.out.println("study java 4 hrs per day");
+        }
+        
+        void  learn()
+        {
+        	System.out.println("learn java");
+        }
+        
+     
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo d=new Demo();
+		d.study();
+		d.learn();
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
