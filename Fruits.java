12c6aa6d511964a639877f2f8d33f5f6d127e6a6
kirangond <kirangond05@gmail.com>
2021-03-26 23:33:03 +0530

    Add files via upload
    inheritance in java

---
+++ Fruits.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class Fruits {
+      int price;
+      String name;
+      void cutFruits()
+      {
+    	  System.out.println("cut the fruits");
+      }
+      void washFruits()
+      {
+    	  System.out.println("wash  the fruits");  
+      }
+}
