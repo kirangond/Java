3c1ebb8e6a2fe0efa1c36556b8ef61cfb0f7f1ca
kirangond <kirangond05@gmail.com>
2021-04-21 22:39:15 +0530

    Add files via upload

---
+++ Star6.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Star6
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=n;j++)
+{
+if(i==1 || i==n || i==j || i+j==n+1 || j==1 || j==n)
+{
+System.out.print("* ");
+}
+else
+{
+System.out.print("  ");
+}
+}
+System.out.println();
+}
+
+
+
+}
+}
