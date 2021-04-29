8f20f683d7321a8801481fe1d7cd45ee6f8fd728
kirangond <kirangond05@gmail.com>
2021-04-29 23:08:56 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+class Calculator
+{
+public static void main(String args[])
+{
+int a=10,b=20;
+System.out.printf("sum is");
+System.out.printf(" %d ",a+b);
+System.out.printf("sub is");
+System.out.printf(" %d ",a-b);
+System.out.printf("product is");
+System.out.printf(" %d ",a*b);
+System.out.printf("division is");
+System.out.printf(" %d ",a/b);
+}
+}
